1d895d06cc84081501464e85f9dcff05807c2f0d
Yuri An <yuriandev@gmail.com>
2023-01-08 16:38:10 +0600

    feat: add the the input element and first pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pipes';
+  title = 'Payment form';
+  name!: string;
+
+  onNameChange(value: string) {
+    this.name = value;
+  }
 }
